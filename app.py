6ad3b598be682d397f17ee99ae19fd1c886b0b50
Takumi Yanai <yanai@example.com>
2025-10-31 05:54:39 +0900

    Fix: create post table on first request

--- app.py
+++ app.py
@@ -37,6 +37,7 @@ if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
 
-@app.before_first_request
+@app.before_first_reques
 def create_tables():
-    db.create_all()
+        db.create_all()
+
