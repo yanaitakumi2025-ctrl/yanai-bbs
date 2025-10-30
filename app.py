7956ca491e21ee6725dc2a09a85b39b01788985f
Takumi Yanai <yanai@example.com>
2025-10-31 07:19:57 +0900

    Add: ログイン・新規登録機能付き掲示板

--- app.py
+++ app.py
@@ -15,6 +15,7 @@ db = SQLAlchemy(app)
 
 login_manager = LoginManager()
 login_manager.init_app(app)
+login_manager.login_view = "login"
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
