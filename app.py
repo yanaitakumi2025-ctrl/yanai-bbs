629fbe3656a868039b11be6f719c2609caf3f372
Takumi Yanai <yanai@example.com>
2025-10-31 05:56:25 +0900

    Fix: typo in before_first_request

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
 
-@app.before_first_reques
+@app.before_first_request
 def create_tables():
         db.create_all()
 
