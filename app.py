2bab79446d1688f63aa86c4a9af649f151754791
Takumi Yanai <yanai@example.com>
2025-10-31 05:44:33 +0900

    Fix: add db.create_all() on first request

--- app.py
+++ app.py
@@ -36,3 +36,7 @@ def add():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
